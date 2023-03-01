1f49c8d837e40a72756e2afd9d5668d3069e15d0
Ash2982 <74827736+Ash2982@users.noreply.github.com>
2023-03-01 12:29:09 +0530

    Add files via upload

---
+++ Base/definitions.sv
@@ -0,0 +1,12 @@
+////////////////////////////// Ashwin Alex George ////////////////////////////
+////////////////////////// AHB APB Base Definitions //////////////////////////
+
+`define hc_duration 5
+`define hr_duration 20
+`define pc_duration 5
+`define pr_duration 20
+
+`define seq_size    20
+
+`define ADDR_WIDTH  32
+`define DATA_WIDTH  32
